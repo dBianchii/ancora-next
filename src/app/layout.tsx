8bd1a5e4747767fed76956651d8449dd267c3b8a
Gabriel Bianchi <gdbianchii@gmail.com>
2023-12-11 23:21:45 -0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter as FontSans } from "next/font/google";
 import { ThemeProvider } from "~/components/providers";
 import { cn } from "~/lib/utils";
 
-export const fontSans = FontSans({
+const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
