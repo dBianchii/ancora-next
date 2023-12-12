4cb7eb9dcb260b14e3a3a17fe872f1b1fb2a4a8a
Gabriel Bianchi <gdbianchii@gmail.com>
2023-12-11 22:41:49 -0300

    shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
+import { buttonVariants } from "~/components/ui/button";
+import { cn } from "~/lib/utils";
 import { getServerAuthSession } from "~/server/auth";
+import { db } from "~/server/db";
 
 export default async function HomePage() {
   const session = await getServerAuthSession();
+  let users = await db.user.findMany();
+  users = [...users, ...users];
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
@@ -11,11 +17,28 @@ export default async function HomePage() {
         </h1>
         <Link
           href={session ? "/api/auth/signout" : "/api/auth/signin"}
-          className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+          className={cn(buttonVariants())}
         >
           {session ? "Sign out" : "Sign in"}
         </Link>
+        {session && (
+          <Avatar>
+            <AvatarImage src={session.user.image ?? ""} alt="@shadcn" />
+            <AvatarFallback>{session.user.name?.split("")[0]}</AvatarFallback>
+          </Avatar>
+        )}
         {session && <p className="text-center">{JSON.stringify(session)}</p>}
+        <h1 className="text-4xl font-bold">
+          Lista de todos os usuarios (exemplo de como usar o prisma):
+        </h1>
+        <div className="flex space-x-4">
+          {users?.map((user) => (
+            <Avatar>
+              <AvatarImage src={user.image ?? ""} alt="@shadcn" />
+              <AvatarFallback>{user.name?.split("")[0]}</AvatarFallback>
+            </Avatar>
+          ))}
+        </div>
       </div>
     </main>
   );
