ffe03370a6c6c180a8f9f95ea658fc4e3c077886
dBianchii <gdbianchii@gmail.com>
2024-03-03 19:06:14 -0300

    commiut

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -24,8 +24,8 @@ export function ReactQueryProvider({
           queries: {
             // With SSR, we usually want to set some default staleTime
             // above 0 to avoid refetching immediately on the client
-            staleTime: 4 * 1000,
-            refetchInterval: 4 * 1000,
+            staleTime: 6 * 1000,
+            refetchInterval: 6 * 1000,
           },
         },
       }),
