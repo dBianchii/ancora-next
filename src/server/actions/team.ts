4d81ec3f4693b2ad48f6a634961d44c7059dd35f
Gabriel Bianchi <gdbianchii@gmail.com>
2024-03-11 08:33:15 -0300

    formatação

--- src/server/actions/team.ts
+++ src/server/actions/team.ts
@@ -1,94 +1,96 @@
 "use server";
 
+import { type Prisma } from "@prisma/client";
 import { db } from "~/server/db";
 import { enforceLoggedIn } from "../utils/enforceLoggedIn";
-import type { ITeam } from "~/app/dashboard/equipes/_components/model";
-import { Prisma } from "@prisma/client";
 
 export const getTeams = async () => {
-	const session = await enforceLoggedIn();
+  const session = await enforceLoggedIn();
 
-	const teams = await db.team.findMany({
-		where: {
-			userId: session.user.id,
-		},
-	});
+  const teams = await db.team.findMany({
+    where: {
+      userId: session.user.id,
+    },
+  });
 
-	return teams;
+  return teams;
 };
 
 export const createTeam = async (teamName: string) => {
-	const session = await enforceLoggedIn();
+  const session = await enforceLoggedIn();
 
-	// verify if teamName exists in db
-	const DoesTeamExists = await db.team.findFirst({
-		where: {
-			userId: session.user.id,
-			name: teamName,
-		},
-	});
+  // verify if teamName exists in db
+  const DoesTeamExists = await db.team.findFirst({
+    where: {
+      userId: session.user.id,
+      name: teamName,
+    },
+  });
 
-	// if teamName exists, throw error
-	if (DoesTeamExists) {
-		throw new Error("O nome da equipe já existe.");
-	}
+  // if teamName exists, throw error
+  if (DoesTeamExists) {
+    throw new Error("O nome da equipe já existe.");
+  }
 
-	// create team
-	const data = {
-		name: teamName,
-		userId: session.user.id,
-		usersEmails: [],
-	};
+  // create team
+  const data = {
+    name: teamName,
+    userId: session.user.id,
+    usersEmails: [],
+  };
 
-	const team = await db.team.create({
-		data,
-	});
+  const team = await db.team.create({
+    data,
+  });
 
-	return team;
+  return team;
 };
 
 export const deleteTeam = async (teamId: string) => {
-	const session = await enforceLoggedIn();
+  const session = await enforceLoggedIn();
 
-	// get all teams of the user
-	const teams = await db.team.findMany({
-		where: {
-			userId: session.user.id,
-		},
-	});
+  // get all teams of the user
+  const teams = await db.team.findMany({
+    where: {
+      userId: session.user.id,
+    },
+  });
 
-	// if the teamId exists, delete it
-	const team = teams.find((team) => team.id === teamId);
-	if (team) {
-		await db.team.delete({
-			where: {
-				id: teamId,
-			},
-		});
-		return;
-	}
-}
+  // if the teamId exists, delete it
+  const team = teams.find((team) => team.id === teamId);
+  if (team) {
+    await db.team.delete({
+      where: {
+        id: teamId,
+      },
+    });
+    return;
+  }
+};
 
+export const updateTeam = async ({
+  teamId,
+  emails,
+}: {
+  teamId: string;
+  emails: string[];
+}) => {
+  try {
+    const validData: Prisma.TeamUpdateInput = {
+      usersEmails: emails,
+    };
 
-export const updateTeam = async ({ teamId, emails }: { teamId: string, emails: string[] }) => {
-	
-	try {
-		const validData: Prisma.TeamUpdateInput = {
-			usersEmails: emails,
-		};
+    const team = await db.team.update({
+      where: {
+        id: teamId,
+      },
+      data: {
+        ...validData,
+      },
+    });
 
-		const team = await db.team.update({
-			where: {
-				id: teamId,
-			},
-			data: {
-				...validData,
-			},
-		});
-
-		return team;
-
-	} catch {
-		throw new Error("Internal Error");
-	}
+    return team;
+  } catch {
+    throw new Error("Internal Error");
+  }
 };
