681ce799ee4c02140e6cb6fd918206c0df05707d
wwwacedo <eng.marianammacedo@gmail.com>
2024-03-15 07:09:42 -0300

    melhorando o eventcard (to be continued)

--- src/app/_components/events-section.tsx
+++ src/app/_components/events-section.tsx
@@ -21,6 +21,7 @@ import {
 import { CreateEventsButton } from "./create-events-button";
 import { useEventsData, useTeamsData } from "./hooks";
 import Link from "next/link";
+import { Settings } from "lucide-react";
 
 import {
   Tabs,
@@ -33,11 +34,11 @@ export function EventsSection() {
   const { query } = useEventsData();
 
   return (
-    <section className="h-full py-6 lg:pl-8 lg:border-l">
-      <h2 className="space-y-1 text-2xl font-semibold tracking-tight pb-4">
+    <section className="h-full py-6 lg:border-l lg:pl-8">
+      <h2 className="space-y-1 text-2xl font-semibold tracking-tight ">
         Meus eventos
       </h2>
-      <Tabs defaultValue="proximos" className="h-full space-y-6" >
+      <Tabs defaultValue="proximos" className="h-full space-y-6">
         <TabsList>
           <TabsTrigger value="proximos">Próximos</TabsTrigger>
           <TabsTrigger value="antigos">Antigos</TabsTrigger>
@@ -85,6 +86,53 @@ function EventCard({
 }: {
   event: Awaited<ReturnType<typeof getEvents>>[number];
 }) {
+  return (
+    <div className="">
+      <div className="group relative">
+        <Link className="group relative" href={`/event/${event.id}`}>
+          <Image
+            alt="Stream thumbnail"
+            className="aspect-[16/9] w-full rounded-lg object-cover transition-transform duration-200 group-hover:z-10 group-hover:scale-[1.05]"
+            height="225"
+            src={
+              event.thumbnailUrl ??
+              "https://generated.vusercontent.net/placeholder.svg"
+            }
+            width="400"
+          />
+          <div className="absolute inset-0 flex items-center justify-center gap-2 opacity-0 transition-opacity duration-200 group-hover:opacity-100">
+            <PlayIcon className="h-10 w-10 rounded-full bg-primary p-2" />
+          </div>
+        </Link>
+      </div>
+      <div className="flex items-center justify-between p-1">
+        <div className=" mt-2 flex flex-col gap-1.5">
+          <h3 className="font-semibold leading-none tracking-tight">
+					{event.title.length > 50 ? event.title.substring(0, 50) + '...' : event.title}
+          </h3>
+          <p className="text-sm leading-none text-gray-500 dark:text-gray-400">
+            Hosted by <span className="font-bold">{event.User.name}</span>
+          </p>
+          <p className="text-sm leading-none text-gray-500 dark:text-gray-400">
+            Starts at{" "}
+            <span className="font-bold">
+              {dayjs(event.datetime).format("DD/MM/YYYY HH:mm")}
+            </span>
+          </p>
+        </div>
+        <div className="w-1/5 flex justify-end">
+          <ConfigEvent event={event} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function ConfigEvent({
+  event,
+}: {
+  event: Awaited<ReturnType<typeof getEvents>>[number];
+}) {
   const { query } = useTeamsData();
 
   const teamEmails = event.invitedPrivateUsers?.flatMap((teamId) => {
@@ -93,75 +141,37 @@ function EventCard({
     return usersEmails ?? [];
   });
 
-	const buttonName = event.isLive 
-  ? "Assistir agora" 
-  : event.datetime < new Date() 
-    ? "Assistir evento gravado" 
-    : "Entrar no evento";
-	
-
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <div className="group relative">
-          <div className="group relative">
-            <Image
-              alt="Stream thumbnail"
-              className="aspect-[16/9] w-full rounded-lg object-cover transition-transform duration-200 group-hover:z-10 group-hover:scale-[1.05]"
-              height="225"
-              src={
-                event.thumbnailUrl ??
-                "https://generated.vusercontent.net/placeholder.svg"
-              }
-              width="400"
-            />
-            <div className="absolute inset-0 flex items-center justify-center gap-2 opacity-0 transition-opacity duration-200 group-hover:opacity-100">
-              <PlayIcon className="h-10 w-10 rounded-full bg-primary p-2" />
-            </div>
-          </div>
-          <div className="mt-2 flex flex-col gap-1.5">
-            <h3 className="font-semibold leading-none tracking-tight">
-              {event.title}
-            </h3>
-            <p className="text-sm leading-none text-gray-500 dark:text-gray-400">
-              Hosted by <span className="font-bold">{event.User.name}</span>
-            </p>
-            <p className="text-sm leading-none text-gray-500 dark:text-gray-400">
-              Starts at{" "}
-              <span className="font-bold">
-                {dayjs(event.datetime).format("DD/MM/YYYY HH:mm")}
-              </span>
-            </p>
-          </div>
-        </div>
+        <Settings className="h-6 w-6 cursor-pointer text-gray-500 dark:text-gray-400" />
       </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle></DialogTitle>
-          </DialogHeader>
-          <Button variant="default">
-            <Link href={`/event/${event.id}`}>
-							{buttonName}
-						</Link>
-          </Button>
-          {teamEmails.length > 0 && (
-            <>
-              <div className="rounded bg-zinc-900 p-4">
-                <h1 className="text-bold mb-4">Convidados:</h1>
-                <div className="h-32 overflow-y-auto">
-                  <TableOfMembers
-                    members={teamEmails ?? []}
-                    removeMember={false}
-                    removeMemberFn={null}
-                  />
-                </div>
+
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Configurações</DialogTitle>
+        </DialogHeader>
+        <p className="text-yellow-600">
+          ToDo: possibilitar alterações do nome/data/thumbnail
+        </p>
+        {teamEmails.length > 0 && (
+          <>
+            <div className="rounded bg-zinc-900 p-4">
+              <h1 className="text-bold mb-4">Convidados:</h1>
+              <div className="h-32 overflow-y-auto">
+                <TableOfMembers
+                  members={teamEmails ?? []}
+                  removeMember={false}
+                  removeMemberFn={null}
+                />
               </div>
-              <Button variant="ghost">
-                Enviar lembrete da reunião via e-mail
-              </Button>
-            </>
-          )}
-        </DialogContent>
+            </div>
+            <Button variant="ghost">
+              Enviar lembrete da reunião via e-mail
+            </Button>
+          </>
+        )}
+      </DialogContent>
     </Dialog>
   );
 }
