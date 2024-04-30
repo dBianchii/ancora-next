442f7817ca6e26e8310c9e8be7267b52fc9f1b91
wwwacedo <eng.marianammacedo@gmail.com>
2024-04-30 06:33:44 -0300

    fix: updating Destino`s page

--- src/app/dashboard/destinos/page.tsx
+++ src/app/dashboard/destinos/page.tsx
@@ -2,10 +2,23 @@ import React from "react";
 
 export default function DestinosPage() {
   return (
-    <section className="h-full py-6 lg:border-l lg:pl-8">
-      <h2 className="mb-2 space-y-1 text-2xl font-semibold tracking-tight ">
-        Destinos
-      </h2>
-    </section>
+    <div className="h-full py-6 lg:border-l lg:pl-8">
+      <div className="mb-2 flex items-center justify-between space-y-1 text-2xl font-semibold tracking-tight">
+        <h2 className="">Destinos</h2>
+      </div>
+      <div className="space-y-4">
+        <Card />
+      </div>
+    </div>
   );
 }
+
+const Card = () => {
+  return (
+    <div className="rounded-xl border bg-card p-6">
+      <div className="flex items-center justify-center gap-x-10">
+        <p className="shrink-0 font-semibold animate-pulse">Feature em desenvolvimento</p>
+      </div>
+    </div>
+  );
+};
