5d20d9cbf968ecaa8f00da343df602c453b2928d
wwwacedo <eng.marianammacedo@gmail.com>
2024-05-07 06:46:19 -0300

    feat: whichMonths function - to be enhanced

--- src/app/dashboard/_components/metrics.ts
+++ src/app/dashboard/_components/metrics.ts
@@ -1,11 +1,36 @@
-
-
-export function howManyMonths(firstEventDatetime: Date | undefined, lastEventDatetime: Date | undefined) {
+export function howManyMonths(
+  firstEventDatetime: Date | undefined,
+  lastEventDatetime: Date | undefined,
+) {
   if (!firstEventDatetime || !lastEventDatetime) {
     return 0;
   }
 
-  const monthsDiff = lastEventDatetime.getMonth() - firstEventDatetime.getMonth() +
-    (12 * (lastEventDatetime.getFullYear() - firstEventDatetime.getFullYear()));
+  const monthsDiff =
+    lastEventDatetime.getMonth() -
+    firstEventDatetime.getMonth() +
+    12 * (lastEventDatetime.getFullYear() - firstEventDatetime.getFullYear());
   return monthsDiff + 1;
+}
+
+export function whichMonths(
+  firstEventDatetime: Date | undefined,
+  lastEventDatetime: Date | undefined,
+): string[] {
+  if (!firstEventDatetime || !lastEventDatetime) {
+    return [];
+  }
+
+  const months: string[] = [];
+  const currentDate = new Date(firstEventDatetime.getTime());
+
+  while (currentDate <= lastEventDatetime) {
+    const month = currentDate.toLocaleString("default", { month: "long" });
+    const year = currentDate.getFullYear();
+    const formattedDate = `${month}/${year}`;
+    months.push(formattedDate);
+    currentDate.setMonth(currentDate.getMonth() + 1);
+  }
+
+  return months;
 }
